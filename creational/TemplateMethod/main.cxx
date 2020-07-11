a88df4277304e50a1dc3c138da6b2c291c0736cb
BorisovSS <borisov.s@7gis.ru>
2020-07-11 13:47:17 +0500

    Рефакторинг

--- creational/TemplateMethod/main.cxx
+++ creational/TemplateMethod/main.cxx
@@ -1,87 +1,55 @@
 #include <iostream>
 
-class Education {
-  public:
-    Education() {}
-    virtual ~Education() {}
-    virtual std::string stepName()  = 0;
-    virtual std::string admission() = 0;
-    virtual std::string studying()  = 0;
-    virtual std::string control()   = 0;
-    virtual std::string awarding()  = 0;
-    void learn()
-    {
-      std::cout <<         stepName()  << std::endl;
-      std::cout << "  " << admission() << std::endl;
-      std::cout << "  " << studying()  << std::endl;
-      std::cout << "  " << control()   << std::endl;
-      std::cout << "  " << awarding()  << std::endl;
-    }
+class Education
+{
+    protected:
+        virtual const char* stepName() const  = 0;
+        virtual const char* admission() const = 0;
+        virtual const char* studying() const = 0;
+        virtual const char* control() const = 0;
+        virtual const char* awarding()  const = 0;
+    public:
+        virtual ~Education() {}
+        void learn() const
+        {
+            std::cout <<         stepName()  << std::endl;
+            std::cout << "  " << admission() << std::endl;
+            std::cout << "  " << studying()  << std::endl;
+            std::cout << "  " << control()   << std::endl;
+            std::cout << "  " << awarding()  << std::endl;
+        }
 };
 
-class School: public Education {
+class School: public Education
+{
+    const char* stepName() const final { return "School"; }
+    const char* admission() const final { return "Admission to school"; }
+    const char* studying() const final { return "Attending lessons"; }
+    const char* control() const final { return "Exams passage"; }
+    const char* awarding() const final { return "Getting school certificate"; }
   public:
-    School() {}
     virtual ~School() {}
-    std::string stepName()
-    {
-      return "School";
-    }
-    std::string admission()
-    {
-      return "Admission to school";
-    }
-    std::string studying()
-    {
-      return "Attending lessons";
-    }
-    std::string control()
-    {
-      return "Exams passage";
-    }
-    std::string awarding()
-    {
-      return "Getting school certificate";
-    }
 };
 
 
-class University: public Education {
+class University: public Education
+{
+    const char* stepName() const final { return "University"; }
+    const char* admission() const final { return "Going to university."; }
+    const char* studying() const final { return "Attending lectures"; }
+    const char* control() const final { return "State examination"; }
+    const char* awarding() const final { return "Getting school diploma"; }
   public:
-    University() {}
     virtual ~University() {}
-    std::string stepName()
-    {
-      return "University";
-    }
-    std::string admission()
-    {
-      return "Going to university.";
-    }
-    std::string studying()
-    {
-      return "Attending lectures";
-    }
-    std::string control()
-    {
-      return "State examination";
-    }
-    std::string awarding()
-    {
-      return "Getting school diploma";
-    }
 };
 
 int main()
 {
-  School     *oSchool     = new School();
-  University *oUniversity = new University();
+  const Education *school     = new School();
+  school->learn();
 
-  oSchool->learn();
-  oUniversity->learn();
-
-  delete oSchool;
-  delete oUniversity;
+  const Education *university = new University();
+  university->learn();
 
   return 0;
 }
